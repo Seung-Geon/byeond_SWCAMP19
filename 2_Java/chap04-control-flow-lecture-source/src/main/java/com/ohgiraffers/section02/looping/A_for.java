1994183886b92a944f215822bd5f62b6a1eb1f3a
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-08-07 09:42:16 +0900

    no message

---
+++ 2_Java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/A_for.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section02.looping;
+
+public class A_for {
+    public void testSimpleForStatement() {
+
+        /* 설명. 1부터 10까지 출력하는 반복문 작성하기 */
+        for(int i = 1;
+            i <= 10;
+            i++) {
+            System.out.println(i + "출력");
+        }
+    }
+}
