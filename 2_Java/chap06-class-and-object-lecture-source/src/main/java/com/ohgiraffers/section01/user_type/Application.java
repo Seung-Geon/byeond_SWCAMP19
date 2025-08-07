ff481e0ce3958de76c00d572d364f97413b50565
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-08-07 19:41:18 +0900

    no message

---
+++ 2_Java/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section01/user_type/Application.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section01.user_type;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Member member1 = new Member();
+        Member member2 = new Member();
+    }
+}
