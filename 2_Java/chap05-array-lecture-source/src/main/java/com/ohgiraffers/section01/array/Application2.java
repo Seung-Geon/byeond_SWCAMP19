0603dcfb559441c5c484576290612cd3df372618
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-08-07 12:49:05 +0900

    배열

---
+++ 2_Java/chap05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/Application2.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section01.array;
+
+import java.util.Arrays;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+        /* 수업목표. 배열의 사용방법을 이혀 배열을 사용할 수 있다. */
+        /* 설명.
+         *  배열의 사용방법
+         *   1. 배열의 선언
+         *   2. 배열의 크기 할당
+         *   3. 배열의 인덱스 공간에 값 대입
+        * */
+
+        /* 설명. 선언과 초기화는 따로 진행 가능 */
+        int[] iArr;
+        char cArr[];        // []의 위치가 변수명 뒤에 있어도 동작O. 하지만 지양
+                            // 가독성이 떨어짐
+        iArr = new int[5];
+        cArr = new char[5];
+
+        /* 설명. 참조 자료형을 출력하면 실제로는 해당 자료형의 toString()이 실행 */
+        System.out.println("iArr = " + iArr);    // 기본 자료형이 아닌 값을 출력하면 toString() 실행
+                                                 // iArr -> iArr.toString()
+        System.out.println("iArr = " + iArr.toString());
+        System.out.println("iArr의 10진수 주소: " + iArr.hashCode());
+        System.out.println("iArr 배열에 담긴 값: " + Arrays.toString(iArr));
+    }
+}
---
+++ 2_Java/chap05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/Application3.java
@@ -0,0 +1,38 @@
+package com.ohgiraffers.section01.array;
+
+import java.util.Arrays;
+
+public class Application3 {
+    public static void main(String[] args) {
+
+        /* 수업목표. 배열에 들어갈 초기 기본값 및 전달인자로써의 배열 자료형 */
+        /* 설명.
+         *  값의 형태별 기본값
+         *  정수: 0
+         *  실수: 0.0
+         *  논리: false
+         *  문자: \u0000, \0
+         *  참조: null
+        * */
+
+        int[] iArr = new int[5];
+        iArr = null;
+        System.out.println(iArr);
+        
+        /* 설명. 크기 및 원하는  값이 채워진 상태의 배열 바로 만들어보기 */
+        int[] iArr2 = {10, 11, 12, 13, 14};
+        int[] iArr3 = new int[]{20, 21, 22, 23, 24};        // 정석, 앞부분이 생략 가능해 위의 형태가 가능
+        
+        System.out.println(Arrays.toString(iArr2));
+        System.out.println(Arrays.toString(iArr3));
+
+        /* 설명. 다만 new 연산자를 생략해서 안되는 경우가 있다. */
+        test(new int[]{10, 11, 12, 13, 14});// 이렇게 배열을 사용하면 메소드가 종료되면 더 이상 사용되지 않으므로 heap 영역에서 삭제된다.
+        //test({10, 11, 12, 13, 14});       // Error. 메소드에 바로 넘기는 경우 new int[] 생략 불가능
+        
+    }
+
+    private static void test(int[] iArr2) {
+        System.out.println("메소드에서 넘겨받은 배열 확인: " + Arrays.toString(iArr2));      // 배열을 준 것이 아니라 배열의 위치를 준 것
+    }
+}
