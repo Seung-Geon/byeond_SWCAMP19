5da60b333f908622f0f5c13be450808ba12736ad
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-08-07 11:11:16 +0900

    제어문

--- 2_Java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
+++ 2_Java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -3,6 +3,21 @@ package com.ohgiraffers.section02.looping;
 public class Application {
     public static void main(String[] args) {
         A_for a = new A_for();
-        a.testSimpleForStatement();
+        //a.testSimpleForStatement();
+        //a.testForExample();
+        //a.testForExample2();
+
+        B_nestedFor b = new B_nestedFor();
+        //b.printGugudanFromTwoToNine();
+        //b.printStar();
+        //b.testForExample();
+
+        C_while c = new C_while();
+        //c.testSimpleWhileStatement();
+        //c.testWhileExample();
+
+        D_doWhile d = new D_doWhile();
+        //d.testSimpleDoWhileStatement();
+        //d.testDoWhileExample();
     }
 }
