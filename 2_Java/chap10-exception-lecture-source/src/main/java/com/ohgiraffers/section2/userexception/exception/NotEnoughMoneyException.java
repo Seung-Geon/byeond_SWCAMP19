2b84e810146a58136547efd958bf79fd7de136a6
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-08-13 15:39:57 +0900

    no message

---
+++ 2_Java/chap10-exception-lecture-source/src/main/java/com/ohgiraffers/section2/userexception/exception/NotEnoughMoneyException.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section2.userexception.exception;
+
+public class NotEnoughMoneyException extends Exception {
+    public NotEnoughMoneyException(String message) {
+        super(message);
+
+    }
+}
---
+++ 2_Java/chap10-exception-lecture-source/src/main/java/com/ohgiraffers/section2/userexception/exception/PriceNegativeException.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.section2.userexception.exception;
+
+public class PriceNegativeException extends Exception {
+    public PriceNegativeException(String message) {
+        super(message);
+    }
+}
