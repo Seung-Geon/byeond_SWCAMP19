38c4dca680ca62ec85598c80902f0a55b77ada9f
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-08-25 11:22:51 +0900

    no message

--- 2_Java/chap17-solid-lecture-source/src/main/java/com/ohgiraffers/section05/dependencyinversion/problem/Application.java
+++ 2_Java/chap17-solid-lecture-source/src/main/java/com/ohgiraffers/section05/dependencyinversion/problem/Application.java
@@ -2,7 +2,7 @@ package com.ohgiraffers.section05.dependencyinversion.problem;
 
 
 /* 설명.
- *  의존성 역전 원칙(Dependency Inversion Principle)
+ *  의존성 역전 원칙(Dependency Inversion Principle) - "자식에게 의존하면 자식 때문에 휘둘려!"
  *  - 고수준 모듈은 저수준 모듈을 의존해서는 안된다.(둘 다 추상화에 의존해야 한다.)
  *  - 추상화는 세부 사항에 의존해서는 안된다. 세부 사항이 추상화에 의존해야 한다.
 * */
