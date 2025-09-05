0d3a9f91bee9c56083d911bbbe532f14f8d5d816
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-09-05 09:49:58 +0900

    no message

---
+++ 06_spring/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section01/autowired/subsection02/setter/Application.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section01.autowired.subsection02.setter;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext("com.ohgiraffers.section01");
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        BookService bookService = (BookService) context.getBean("setterService", BookService.class);    // 동일 타입이 여러개 있으면 이렇게 사용
+//        System.out.println("bookService = " + bookService);
+
+        bookService.findAllBook().forEach(System.out::println);
+
+        System.out.println("1번 책: " + bookService.findBookBySequenceOf(1));
+        System.out.println("2번 책: " + bookService.findBookBySequenceOf(2));
+        System.out.println("3번 책: " + bookService.findBookBySequenceOf(3));
+    }
+}
---
+++ 06_spring/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section01/autowired/subsection02/setter/BookService.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section01.autowired.subsection02.setter;
+
+import com.ohgiraffers.section01.common.BookDAO;
+import com.ohgiraffers.section01.common.BookDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("setterService")
+public class BookService {
+
+    @Autowired                  // 자동으로 주입 받음
+    private BookDAO bookDAO;
+
+    public List<BookDTO> findAllBook() {
+        return bookDAO.findAllBook();
+    }
+
+    public BookDTO findBookBySequenceOf(int sequence) {
+        return bookDAO.findBookBySequenceOf(sequence);
+    }
+}
