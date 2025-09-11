1ff3c139c234931492c46ae845f1703f3d158a13
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-09-11 12:15:53 +0900

    no message

---
+++ 08_mybatis/chap02-crud-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/MenuController.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import java.util.List;
+
+public class MenuController {
+
+    private final MenuService menuService;
+    private final PrintResult printResult;  // 응답 출력용 페이지의 느낌으로 만든 클래스
+
+    public MenuController() {
+        this.menuService = new MenuService();
+        this.printResult = new PrintResult();
+    }
+
+    public void findAllMenus() {
+        List<MenuDTO> menuList = menuService.findAllMenus();
+
+        if(!menuList.isEmpty()) printResult.printMenus(menuList);   // 하나의 메뉴라도 조회됨
+        else printResult.printErrormessage("전체 메뉴 조회 실패");     // 조회 실패(하나의 메뉴도 없는 상태)
+    }
+}
