3661ef0e29330ae0f9f7a8e72236c5800bd1f06d
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-09-12 14:22:56 +0900

    no message

--- 08_mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/dynamic/Application.java
+++ 08_mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/dynamic/Application.java
@@ -1,6 +1,5 @@
 package com.ohgiraffers.dynamic;
 
-import java.util.Map;
 import java.util.Scanner;
 
 public class Application {
@@ -19,6 +18,12 @@ public class Application {
             switch (input) {
                 case 1:
                     ifSubMenu();
+                    break;
+                case 2:
+                    chooseSubMenu();
+                    break;
+                case 3:
+
                     break;
                 case 9:
                     System.out.println("프로그램을 종료합니다.");
@@ -51,8 +56,6 @@ public class Application {
                     return;
             }
         } while (true);
-
-
     }
 
     private static int inputPrice() {
@@ -70,5 +73,33 @@ public class Application {
 
         return new  SearchCriteria(condition, value);
 
+    }
+
+    private static void chooseSubMenu() {
+        Scanner sc = new Scanner(System.in);
+        MenuService ms = new MenuService();
+
+        do {
+            System.out.println("==== choose 서브 메뉴 ====");
+            System.out.println("1. 카테고리 상위 분류별 메뉴 보여주기(식사, 음료, 디저트)");
+            System.out.println("9. 이전 메뉴로");
+            System.out.print("메뉴 번호를 입력해 주세요: ");
+            int input = sc.nextInt();
+            switch (input) {
+                case 1:
+                    ms.searchMenuBySupCategory(inputSupCategory());
+                    break;
+                case 9:
+                    return;
+            }
+        } while (true);
+    }
+
+    private static SearchCriteria inputSupCategory() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("메뉴의 상위 분류를 입력해 주세요(식사, 음료, 디저트): ");
+        String value = sc.nextLine();
+
+        return new SearchCriteria("category", value);
     }
 }
