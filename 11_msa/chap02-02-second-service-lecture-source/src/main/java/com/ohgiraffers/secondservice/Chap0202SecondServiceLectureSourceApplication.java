d9ef0fa2a7aa9fbcc145d4134d7a62ade4bdb67b
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-09-23 12:47:13 +0900

    no message

--- 11_msa/chap02-02-second-service-lecture-source/src/main/java/com/ohgiraffers/secondservice/Chap0202SecondServiceLectureSourceApplication.java
+++ 11_msa/chap02-02-second-service-lecture-source/src/main/java/com/ohgiraffers/secondservice/Chap0202SecondServiceLectureSourceApplication.java
@@ -13,3 +13,4 @@ public class Chap0202SecondServiceLectureSourceApplication {
 	}
 
 }
+
--- 11_msa/charp02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
+++ 11_msa/charp02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+//@RequestMapping("/first-service")
 public class FirstServiceController {
     /* 설명. @Value와 같이 application.yml 또는 컴퓨터 환경 변수를 불러오기 위한 객체 */
     private Environment env;
