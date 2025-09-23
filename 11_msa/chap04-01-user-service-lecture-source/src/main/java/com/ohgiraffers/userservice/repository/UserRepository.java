546bb2a51c6c47fa397f9ac46a69f72a4f303203
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-09-23 15:48:53 +0900

    no message

---
+++ 11_msa/chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.userservice.repository;
+
+import com.ohgiraffers.userservice.aggregate.UserEntity;
+import com.ohgiraffers.userservice.dto.UserDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
