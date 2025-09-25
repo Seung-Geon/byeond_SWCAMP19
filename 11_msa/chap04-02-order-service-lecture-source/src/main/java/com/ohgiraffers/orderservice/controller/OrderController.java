c3553d13e7c1c104b0840149f546897915a9ad5c
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-09-25 10:32:15 +0900

    no message

--- 11_msa/chap04-02-order-service-lecture-source/src/main/java/com/ohgiraffers/orderservice/controller/OrderController.java
+++ 11_msa/chap04-02-order-service-lecture-source/src/main/java/com/ohgiraffers/orderservice/controller/OrderController.java
@@ -1,10 +1,15 @@
 package com.ohgiraffers.orderservice.controller;
 
+import com.ohgiraffers.orderservice.dto.OrderDTO;
+import com.ohgiraffers.orderservice.dto.ResponseOrderDTO;
 import com.ohgiraffers.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -15,6 +20,10 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @GetMapping("/users/{memNo}/orders")
-    public ResponseEntity<>
+    @GetMapping("/users/{userId}/orders")
+    public ResponseEntity<List<ResponseOrderDTO>> getUserOrders(@PathVariable int userId) {
+        List<OrderDTO> orderDTOList = orderService.getOrderByUserId(userId);
+
+        return null;
+    }
 }
--- 11_msa/chap04-02-order-service-lecture-source/src/main/java/com/ohgiraffers/orderservice/service/OrderServiceImpl.java
+++ 11_msa/chap04-02-order-service-lecture-source/src/main/java/com/ohgiraffers/orderservice/service/OrderServiceImpl.java
@@ -1,7 +1,27 @@
 package com.ohgiraffers.orderservice.service;
 
+import com.ohgiraffers.orderservice.dto.OrderDTO;
+import com.ohgiraffers.orderservice.repository.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class OrderServiceImpl {
+@Slf4j
+public class OrderServiceImpl implements OrderService {
+
+    private OrderMapper orderMapper;
+
+    @Autowired
+    public void setOrderMapper(OrderMapper orderMapper) {
+        this.orderMapper = orderMapper;
+    }
+
+    @Override
+    public List<OrderDTO> getOrderByUserId(int userId) {
+        log.info("order 서비스에서 출력: {}", orderMapper.selectOrderByUserId(userId));
+        return null;
+    }
 }
