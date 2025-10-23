3a5e22edbb4a0a4188198a09f402f7434907a943
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-10-23 12:23:24 +0900

    no message

--- 15_vue/07_slot/src/App.vue
+++ 15_vue/07_slot/src/App.vue
@@ -8,6 +8,7 @@
   import MyComponent from '@/component/01_component/MyComponent.vue';     // @로 시작하면 src 부터 시작하는 일종의 절대 경로
   import SlotParent from './component/02_slotintro/SlotParent.vue';
   import NamedParent from './component/03_namedslot/NamedParent.vue';
+  import DynamicParent from './component/04_dynamicslot/DynamicParent.vue'
 </script>
 
 <template>
@@ -19,6 +20,7 @@
   <main>
     <SlotParent/>
     <NamedParent/>
+    <DynamicParent/>
   </main>
 </template>
 
