b307f198cb1baf8f3f7c994a63362639ae2e4c71
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-10-23 12:08:51 +0900

    no message

--- 15_vue/07_slot/src/App.vue
+++ 15_vue/07_slot/src/App.vue
@@ -6,6 +6,8 @@
 <script setup>
   // import MyComponent from './component/01_component/MyComponent.vue';  // 상대 경로
   import MyComponent from '@/component/01_component/MyComponent.vue';     // @로 시작하면 src 부터 시작하는 일종의 절대 경로
+  import SlotParent from './component/02_slotintro/SlotParent.vue';
+  import NamedParent from './component/03_namedslot/NamedParent.vue';
 </script>
 
 <template>
@@ -15,7 +17,8 @@
     </div>
   </header>
   <main>
-
+    <SlotParent/>
+    <NamedParent/>
   </main>
 </template>
 
