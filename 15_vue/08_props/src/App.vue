0c25fff8e888161dc8e343b9f4e83b1a746a6e19
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-10-23 12:44:41 +0900

    no message

---
+++ 15_vue/08_props/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <PropsParent/>
+  </header>
+  <main>
+
+  </main>
+</template>
+
+<script setup>
+  import PropsParent from './components/01_propsBasic/PropsParent.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ 15_vue/08_props/src/components/01_propsBasic/Props.Child.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h3>전달 받은 props: {{ props.message }}</h3>
+    </div>
+</template>
+
+<script setup>
+    import {defineProps} from 'vue';
+
+    // 변숫 수정이 부모에게 영향을 주지 않게 하기 위한 추가 작업
+    const props = defineProps({
+        message: String
+    });
+
+        /* 1. 기본 자료형 (Primitive Types) */
+    // const props = defineProps({
+    //     message: String,
+    //     count: Number,
+    //     isActive: Boolean,
+    //     items: Array,
+    //     user: Object,
+    //     createdAt: Date,
+    //     callback: Function,
+    //     id: Symbol
+    // });
+
+    // 권장X, 복잡해짐
+    /* 2. 여러 타입 허용 */
+    // const props = defineProps({
+    //     value: [String, Number],
+    //     data: [Array, Object]
+    // });
+</script>
+
+<style scoped>
+
+</style>
