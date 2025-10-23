98a944a7c72965af6ae8fdec14a9d1f158e1df83
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-10-23 11:29:45 +0900

    no message

--- 15_vue/07_slot/src/App.vue
+++ 15_vue/07_slot/src/App.vue
@@ -1,11 +1,27 @@
-<script setup></script>
+<!-- 
+  다른 파일을 불러올 때는 상대경로(./) 또는 src부터의 경로인 절대경로(@)를 사용할 수 있다.
+
+  상황에 따라 더 유리한 방법을 선택
+-->
+<script setup>
+  // import MyComponent from './component/01_component/MyComponent.vue';  // 상대 경로
+  import MyComponent from '@/component/01_component/MyComponent.vue';     // @로 시작하면 src 부터 시작하는 일종의 절대 경로
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <div class = 'redColor'>
+      <MyComponent/>
+    </div>
+  </header>
+  <main>
+
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .redColor {
+    color: red;
+
+  }
+</style>
--- 15_vue/07_slot/src/main.js
+++ 15_vue/07_slot/src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue' // const {createApp} = Vue; 하던 것을 import를 통해 다른 파일에 있던 것을 가져옴
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
