fc6d02033b99021f64d6ce581cc42a4b6097f571
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-10-23 15:13:19 +0900

    no message

--- 15_vue/08_props/src/App.vue
+++ 15_vue/08_props/src/App.vue
@@ -3,12 +3,13 @@
     <PropsParent/>
   </header>
   <main>
-
+    <DrillingParent/>
   </main>
 </template>
 
 <script setup>
   import PropsParent from './components/01_propsBasic/PropsParent.vue';
+  import DrillingParent from './components/02_propsDrilling/DrillingParent.vue';
 </script>
 
 <style scoped>
---
+++ 15_vue/08_props/src/components/02_propsDrilling/DrillingGrandChild.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>GrandChild: {{ injectedCount }}</h3>
+        <button @click = "injectedCount++;">GrandChild에서 값 증가</button> <!-- 하위 컴포넌트가 상위 컴포넌트의 상태를 변경함 -->
+    </div>
+</template>
+
+<script setup>
+    import {inject} from 'vue'
+
+    const injectedCount = inject('count');  // 직접 접근
+                                            // 이런 방식 지양
+                                            // 개발은 편하지만 유지보수가 어려움
+                                            // provide에 담은 변수들을 사용하는 것들을 찾을 때 일일이 찾아야 한다.
+                                            // getter
+</script>
+
+<style scoped>
+
+</style>
