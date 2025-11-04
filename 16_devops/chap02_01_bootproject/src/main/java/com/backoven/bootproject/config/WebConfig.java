045e91af2c39f756bf613955116024df15af013d
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-11-04 10:47:15 +0900

    no message

--- 16_devops/chap02_01_bootproject/src/main/java/com/backoven/bootproject/config/WebConfig.java
+++ 16_devops/chap02_01_bootproject/src/main/java/com/backoven/bootproject/config/WebConfig.java
@@ -1,9 +1,10 @@
 package com.backoven.bootproject.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
+ @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
